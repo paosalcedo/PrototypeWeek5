19f821ab4910ceda3b76135b2fed1c7ef5f1eafe
pao_s <paosalcedo@gmail.com>
2018-02-25 22:49:00 -0500

    Cooking and saucing visual process works

--- WinterOlympicsProject/Assets/Scripts/Edible.cs
+++ WinterOlympicsProject/Assets/Scripts/Edible.cs
@@ -1,25 +1,73 @@
-﻿using System.Collections;
+﻿using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Edible : MonoBehaviour
 {
-
 	public Transform myTransform;
 
+	public MeatState meatState;
+
+	[SerializeField] private MeshRenderer[] meatRenderers;
+
+	public bool isDipped = false;
+
+	[SerializeField]private float timeOnGrill = 0;
+
+	
 	void Awake()
 	{
 		myTransform = transform;
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		isDipped = false;
+		meatState = MeatState.Raw;
 		Utils.RandomizeRotation(Vector3.right, Random.Range(0, 360), this.transform);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (meatState)
+		{
+			case MeatState.Raw:
+				if (timeOnGrill >= 10f && timeOnGrill < 20f)
+				{
+					GetComponent<MeshRenderer>().enabled = false;
+					meatState = MeatState.Cooked;
+				}
+
+				break;
+			case MeatState.Cooked:
+				meatRenderers[0].enabled = true;
+				if (timeOnGrill >= 20f && timeOnGrill < 30f)
+				{
+					meatRenderers[0].enabled = false;
+					meatState = MeatState.Burned;					
+				}
+				break;
+			case MeatState.Burned:
+				meatRenderers[1].enabled = true;
+				if (timeOnGrill >= 30f)
+				{
+//					Destroy(gameObject);	
+				}
+				break;
+			default:
+				break;
+		}
+
+		if (isDipped)
+		{
+			GetComponent<MeshRenderer>().enabled = false;
+			meatRenderers[0].enabled = false;
+			meatRenderers[1].enabled = false;
+			meatRenderers[2].enabled = true;
+		}
+
 	}
 
 	void OnCollisionEnter(Collision coll)
@@ -41,8 +89,17 @@ public class Edible : MonoBehaviour
 		}
 	}
 
+	
 	void OnTriggerStay(Collider trigger)
 	{
+		if (trigger.gameObject.layer == 10)
+		{
+			timeOnGrill += Time.deltaTime;		
+		} else if (trigger.gameObject.layer == 11)
+		{
+			isDipped = true;
+		}
+
 	}
 	
 }
